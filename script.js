8696a2cf9547c90ebacbd31f5f7869a0d67f61bc
ElzZzoz <hazemsoltan274@gmail.com>
2025-02-22 19:05:08 +0200

    Your commit message

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 'use strict';
-<<<<<<< HEAD
-=======
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -44,35 +42,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
-/* the challenge */
-
-// document.querySelector('.again').addEventListener('click', function () {
-//   score = 20;
-//   secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-//   // document.querySelector('.message').textContent = 'Start guessing...';
-//   displayMessage('Start guessing...');
-//   document.querySelector('.score').textContent = score;
-//   document.querySelector('.number').textContent = '?';
-//   document.querySelector('.guess').value = '';
-
-//   document.querySelector('body').style.backgroundColor = '#222';
-//   document.querySelector('.number').style.width = '15rem';
-// });
-
-///////////////////////////////////////
-// Coding Challenge #1
-
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
-
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-GOOD LUCK 😀
-*/
+// again reset the game
 
 const again = document
   .querySelector('.again')
@@ -86,4 +56,3 @@ const again = document
     document.querySelector('.guess').value = '';
     document.querySelector('body').style.backgroundColor = '#222';
   });
->>>>>>> 5ce7d01 (Initial commit)
